00d62eed4d869af5ee25b44169f36fb36669fe2c
imajim <contact@imajim.com>
2019-10-02 17:49:21 +0200

    Update NotifyAction.php

--- src/Action/NotifyAction.php
+++ src/Action/NotifyAction.php
@@ -10,7 +10,7 @@
  * file that was distributed with this source code.
  */
 
-namespace Triotech\SyliusPayboxBundle\Action;
+namespace Imajim\SyliusPayboxBundle\Action;
 
 use Payum\Core\Action\ActionInterface;
 use Payum\Core\Bridge\Spl\ArrayObject;
